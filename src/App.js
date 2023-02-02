8d4fc29373365bec8ba9f273cda66b917531a7f0
rumanak21 <rumana2k20@gmail.com>
2023-02-02 19:38:19 +0600

    Drop down menu updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home/Home';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className="bg-[#030812]">
-      <header className="App-header">
-        <Routes>
-        <Route path="/" element={<Home></Home>} ></Route>
-        </Routes>
-        
-      </header>
+    <div className=" bg-[#030812] ">
+      <div className="container mx-auto  ">
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+          </Routes>
+        </header>
+      </div>
     </div>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -23,42 +23,25 @@ const Home = () => {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            className=" text-[#030812] menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52 "
           >
             <li>
-              <a>Item 1</a>
+              <a>Collections</a>
             </li>
             <li tabIndex={0}>
               <a className="justify-between">
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                </svg>
+              Feature
               </a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
             </li>
             <li>
-              <a>Item 3</a>
+              <a>FAQ</a>
             </li>
           </ul>
         </div>
         <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+        <ul className="menu menu-horizontal px-1 ">
           <li>
             <a>Collections</a>
           </li>

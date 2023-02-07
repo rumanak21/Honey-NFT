a7fda38ccbee174d951167e11c55344ee2f5af03
rumanak21 <rumana2k20@gmail.com>
2023-02-07 23:47:16 +0600

    branding section completed

---
+++ src/pages/Home/BrandingSection.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo1 from "./../../img/Binance_logo logo.svg";
+import logo3 from "./../../img/Ethereum logo.svg";
+import logo2 from "./../../img/Logo_of_Blockchain.io logo.svg";
+
+const BrandingSection = () => {
+  return (
+    <div className="flex flex-col-3 gap-20 w-4/5 m-10 justify-center content-center">
+      <img className="w-48" src={logo1} alt="" />
+      <img  className="w-40" src={logo3} alt="" />
+      <img className="w-56" src={logo2} alt="" />
+      
+    </div>
+  );
+};
+
+export default BrandingSection;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import BrandingSection from "./BrandingSection";
 import HerroSection from "./HerroSection";
 import Navbar from "./Navbar";
 
@@ -7,6 +8,7 @@ const Home = () => {
     <>
     <Navbar></Navbar>
     <HerroSection></HerroSection>
+    <BrandingSection></BrandingSection>
     </>
   );
 };

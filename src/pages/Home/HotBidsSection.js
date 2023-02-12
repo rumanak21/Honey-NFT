c01e29a52031215ddd1d016e850a537b02cb5797
rumanak21 <rumana2k20@gmail.com>
2023-02-12 23:58:14 +0600

    hot bids section design completed

--- src/pages/Home/HotBidsSection.js
+++ src/pages/Home/HotBidsSection.js
@@ -1,47 +1,192 @@
 import React from "react";
+import hot_pic1 from "./../../img/bids-img1.png";
+import hot_pic2 from "./../../img/bids-img2.png";
+import hot_pic3 from "./../../img/bids-img3.png";
+import pro_pic1 from "./../../img/pro-pic1.png";
+import pro_pic2 from "./../../img/pro-pic2.png";
+import pro_pic3 from "./../../img/pro-pic3.png";
 
 const HotBidsSection = () => {
   return (
-    <div className="bg-gray-200 p-10">
-      <h1 className="text-5xl font-bold text-center">Heading</h1>
-      <p className="text-xl font-medium text-center mt-5">Subheading</p>
-      <div className="flex flex-wrap mt-10">
-        <div className="w-full md:w-1/3 p-5">
-          <img src="https://via.placeholder.com/500x500" alt="Image 1" />
-          <div className="mt-5">
-            <p className="text-lg font-medium">User Name</p>
-            <img
-              src="https://via.placeholder.com/50x50"
-              alt="User Pic"
-              className="w-10 h-10 rounded-full"
-            />
+    <div className="">
+      <h1 className="text-3xl font-bold ">Hot Bids</h1>
+      <p className=" text-base font-bold text-[#818388] mt-5 mb-5">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+      <div className="flex flex-wrap gap-8">
+        <div className="card w-80 bg-white bg-opacity-10 shadow-xl">
+          <figure>
+            <img src={hot_pic1} alt="Sassy Bull" />
+          </figure>
+          <div>
+            <div className=" flex flex-col-2 pl-3 pr-3 ">
+              <div className="w-6/12 mt-3 ">
+                <h2 className="mb-3">Sassy Bull</h2>
+                <div>
+                  <div className="flex mb-3 justify-center content-center">
+                    <div className="w-20 rounded-xl">
+                      <img src={pro_pic1} alt="user pic" />
+                    </div>
+                    <p className=" text-center pl-1">@jhonartist</p>
+                  </div>
+
+                  <p className="text-base font-bold text-[#818388] ">
+                    Reserve price
+                  </p>
+                  <div className="flex text-base font-bold text-[#818388] ">
+                    <p>ETH 9.31</p>
+                    <p>($29,8846)</p>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-6 pl-3 text-center ">
+                <p className="text-base font-bold text-[#818388]">
+                  Auction Ends in
+                </p>
+                <div className="flex flex-col-3 p-2">
+                  <div>
+                    <p className="font-bold">09</p>
+                    <p className="text-base font-bold text-[#818388] pr-2">
+                      Hours
+                    </p>
+                  </div>
+                  <div>
+                    <p className="font-bold mr-2 ">15</p>
+                    <p className="text-base font-bold text-[#818388] pr-2">
+                      Mins
+                    </p>
+                  </div>
+                  <div>
+                    <p className="font-bold">23</p>
+                    <p className="text-base font-bold text-[#818388]">Secs</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="card-actions justify-end m-2">
+              <button className="btn bg-gradient-to-t from-primary to-secondary w-28 h-10 ">
+                Place Bid
+              </button>
+            </div>
           </div>
-          <p className="text-sm mt-5">Text 1</p>
         </div>
-        <div className="w-full md:w-1/3 p-5">
-          <img src="https://via.placeholder.com/500x500" alt="Image 2" />
-          <div className="mt-5">
-            <p className="text-lg font-medium">User Name</p>
-            <img
-              src="https://via.placeholder.com/50x50"
-              alt="User Pic"
-              className="w-10 h-10 rounded-full"
-            />
+        <div className="card w-80 bg-white bg-opacity-10 shadow-xl">
+          <figure>
+            <img src={hot_pic2} alt="White Art" />
+          </figure>
+          <div>
+            <div className=" flex flex-col-2 pl-3 pr-3 ">
+              <div className="w-6/12 mt-3 ">
+                <h2 className="mb-3">White Art</h2>
+                <div>
+                  <div className="flex mb-3 justify-center content-center">
+                    <div className="w-20 rounded-xl">
+                      <img src={pro_pic2} alt="user pic" />
+                    </div>
+                    <p className=" text-center pl-1">@jduuensc</p>
+                  </div>
+
+                  <p className="text-base font-bold text-[#818388] ">
+                    Reserve price
+                  </p>
+                  <div className="flex text-base font-bold text-[#818388] ">
+                    <p>ETH 9.73</p>
+                    <p>($75,8463)</p>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-6 pl-3 text-center ">
+                <p className="text-base font-bold text-[#818388]">
+                  Auction Ends in
+                </p>
+                <div className="flex flex-col-3 p-2">
+                  <div>
+                    <p className="font-bold">09</p>
+                    <p className="text-base font-bold text-[#818388] pr-2">
+                      Hours
+                    </p>
+                  </div>
+                  <div>
+                    <p className="font-bold mr-2 ">15</p>
+                    <p className="text-base font-bold text-[#818388] pr-2">
+                      Mins
+                    </p>
+                  </div>
+                  <div>
+                    <p className="font-bold">23</p>
+                    <p className="text-base font-bold text-[#818388]">Secs</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="card-actions justify-end m-2">
+              <button className="btn bg-gradient-to-t from-primary to-secondary w-28 h-10 ">
+                Place Bid
+              </button>
+            </div>
           </div>
-          <p className="text-sm mt-5">Text 2</p>
         </div>
-        <div className="w-full md:w-1/3 p-5">
-          <img src="https://via.placeholder.com/500x500" alt="Image 3" />
-          <div className="mt-5">
-            <p className="text-lg font-medium">User Name</p>
-            <img
-              src="https://via.placeholder.com/50x50"
-              alt="User Pic"
-              className="w-10 h-10 rounded-full"
-            />
+        <div className="card w-80 bg-white bg-opacity-10 shadow-xl">
+          <figure>
+            <img src={hot_pic3} alt="Colour art" />
+          </figure>
+          <div>
+            <div className=" flex flex-col-2 pl-3 pr-3 ">
+              <div className="w-6/12 mt-3 ">
+                <h2 className="mb-3">Colour art</h2>
+                <div>
+                  <div className="flex mb-3 justify-center content-center">
+                    <div className="w-20 rounded-xl">
+                      <img src={pro_pic3} alt="user pic" />
+                    </div>
+                    <p className=" text-center pl-1">@hasyrgsh</p>
+                  </div>
+
+                  <p className="text-base font-bold text-[#818388] ">
+                    Reserve price
+                  </p>
+                  <div className="flex text-base font-bold text-[#818388] ">
+                    <p>ETH 5.81</p>
+                    <p>($21,6743)</p>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-6 pl-3 text-center ">
+                <p className="text-base font-bold text-[#818388]">
+                  Auction Ends in
+                </p>
+                <div className="flex flex-col-3 p-2">
+                  <div>
+                    <p className="font-bold">09</p>
+                    <p className="text-base font-bold text-[#818388] pr-2">
+                      Hours
+                    </p>
+                  </div>
+                  <div>
+                    <p className="font-bold mr-2 ">15</p>
+                    <p className="text-base font-bold text-[#818388] pr-2">
+                      Mins
+                    </p>
+                  </div>
+                  <div>
+                    <p className="font-bold">23</p>
+                    <p className="text-base font-bold text-[#818388]">Secs</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="card-actions justify-end m-2">
+              <button className="btn bg-gradient-to-t from-primary to-secondary w-28 h-10 ">
+                Place Bid
+              </button>
+            </div>
           </div>
-          <p className="text-sm mt-5">Text 3</p>
         </div>
+      </div>
+      <div className="flex justify-center">
+        <button className="btn border-secondary text-secondary hover:text-white hover:bg-gradient-to-t from-primary to-secondary w-36 h-10 m-10 ">
+          Veiw All
+        </button>
       </div>
     </div>
   );

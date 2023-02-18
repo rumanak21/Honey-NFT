a15987e756efbcaf46286b43447a7f26a4542103
rumanak21 <rumana2k20@gmail.com>
2023-02-18 23:42:31 +0600

    FAQ Section completed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,6 +6,7 @@ import HerroSection from "./HerroSection";
 import HotBidsSection from "./HotBidsSection";
 import MoreNFT from "./MoreNFT";
 import Navbar from "./Navbar";
+import QuestionSection from "./QuestionSection";
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
     <HotBidsSection></HotBidsSection>
     <MoreNFT></MoreNFT>
     <CreatorPage/>
+    <QuestionSection></QuestionSection>
     </div>
   );
 };
---
+++ src/pages/Home/QuestionSection.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+const QuestionSection = () => {
+  return (
+    <div className="m-5">
+      <div className="m-10">
+        <h1 className="text-3xl text-center font-bold ">
+          Freaquently Asked Question
+        </h1>
+        <h1 className="text-3xl text-center font-bold ">Question</h1>
+      </div>
+      <div className="grid grid-cols-2 gap-5 ">
+        <div
+          tabIndex={0}
+          className="collapse collapse-plus border border-base-300 rounded-box"
+        >
+          <div className="collapse-title text-base font-bold mb-3 text-[#818388] border-b-2 border-[#818388] ">
+            Lorem ipsum ipsum ?
+          </div>
+          <div className="collapse-content">
+            <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+          </div>
+        </div>
+        <div
+          tabIndex={0}
+          className="collapse collapse-plus border border-base-300 rounded-box"
+        >
+          <div className="collapse-title text-base font-bold mb-3 text-[#818388] border-b-2 border-[#818388] ">
+            Lorem ipsum ipsum ?
+          </div>
+          <div className="collapse-content">
+            <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+          </div>
+        </div>
+        <div
+          tabIndex={0}
+          className="collapse collapse-plus border border-base-300 rounded-box"
+        >
+          <div className="collapse-title text-base font-bold mb-3 text-[#818388] border-b-2 border-[#818388] ">
+            Lorem ipsum ipsum ?
+          </div>
+          <div className="collapse-content">
+            <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+          </div>
+        </div>
+        <div
+          tabIndex={0}
+          className="collapse collapse-plus border border-base-300 rounded-box"
+        >
+          <div className="collapse-title text-base font-bold mb-3 text-[#818388] border-b-2 border-[#818388] ">
+            Lorem ipsum ipsum ?
+          </div>
+          <div className="collapse-content">
+            <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+          </div>
+        </div>
+        <div
+          tabIndex={0}
+          className="collapse collapse-plus border border-base-300 rounded-box"
+        >
+          <div className="collapse-title text-base font-bold mb-3 text-[#818388] border-b-2 border-[#818388] ">
+            Lorem ipsum ipsum ?
+          </div>
+          <div className="collapse-content">
+            <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+          </div>
+        </div>
+        <div
+          tabIndex={0}
+          className="collapse collapse-plus border border-base-300 rounded-box"
+        >
+          <div className="collapse-title text-base font-bold mb-3 text-[#818388] border-b-2 border-[#818388] ">
+            Lorem ipsum ipsum ?
+          </div>
+          <div className="collapse-content">
+            <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionSection;
